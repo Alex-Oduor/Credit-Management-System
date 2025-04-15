df229d6617e3c43d5d03824452720b5e799b1d7f
Alex-Oduor <alexoduor183@gmail.com>
2025-04-15 13:46:51 +0000

    revised show records function

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+
 //Data organization
 struct Customer{
     int customer_ID;
@@ -9,27 +11,27 @@ struct Customer{
     float credit_limit;
     float current_balance;
 };
+
+
 //customers pointer
 struct Customer *customer = NULL;
 int customer_count = 0;
-//adding new customer
 
+//adding new customer
 void addcustomer(){
     struct Customer new_customer;
 
     new_customer .customer_ID = customer_count + 1;
-    printf("Enter customer.name")
+    printf("Enter customer.name");
     getchar();
 }
-void showrecords(struct main customer[],int size){
-    for(int i=0;i<size;i++){
-    printf("CustomerID: %d\n",customer[i].customerID);
-    printf("Customer Name: %s\n",customer[i].name);
-    printf("Customer balance: %.2f\n----------------\n",customer[i].balance);
-     }
+
+// showing records
+void showrecords(){
+  FILE *file=fopen("customer_data.txt","rb");
+
 }
-    };
-    int size= sizeof(customer)/sizeof(customer[0]);
+int main(){
 
     //program initialization
 
@@ -43,22 +45,25 @@ void showrecords(struct main customer[],int size){
     printf("1.Add customer.\n");
     printf("2.Record payment\n");
     printf("3.View outstanding balance\n");
-
+    
+    int option;
     scanf("%d",&option);
 
     //option conditions
     if(option==1){
-        printf("23");
+        //printf("");
     }
     else if(option==2){
         //recordpayment();
 
     }
     else if(option==3){
-        showrecords(customer,size);
+        //showrecords(customer,size);
     }
 
-    else{printf("Invalid option!! Please try again.");}
+    else{
+        printf("Invalid option!! Please try again.");
+    }
 
 
 
